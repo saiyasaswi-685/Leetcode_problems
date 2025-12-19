bacfb32ec56c87a3e62358e179d32f25735967f4
Sai Yasaswi Bandaru <147792291+saiyasaswi-685@users.noreply.github.com>
2025-12-19 22:44:13 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (25.83%) - LeetSync

---
+++ 3676-smallest-number-with-all-set-bits/smallest-number-with-all-set-bits.cpp
@@ -0,0 +1,12 @@
+
+class Solution {
+public:
+    int smallestNumber(int n) {
+        int res=1;
+        while(res <n)
+        {
+            res = res *2 +1;
+        }
+        return res;
+    }
+};
